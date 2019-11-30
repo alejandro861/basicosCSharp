f41448b6f1a85229d83c1b5e5130786e3177f9e5
alejandro861 <gmalejan861@gmail.com>
2019-11-30 09:48:11 -0600

    Se trabaja el ciclo For C#
    Noveno Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -75,6 +75,12 @@ namespace ConsoleApp1
                 rondasFaltantes--;
             } while (rondasFaltantes > 0);
             ////
+            string[] diasSemanales = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+            for (int dia = 0; dia < 7; dia++)
+            {
+                Console.WriteLine("{0} es el número {1} de la semana", diasSemanales[dia], dia + 1);
+            }
+            ////
             
         }
     }
