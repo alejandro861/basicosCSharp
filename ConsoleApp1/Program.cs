ebf47643ee0a1f96a407270f58e9bfb4e14e6a22
alejandro861 <gmalejan861@gmail.com>
2019-11-30 09:37:36 -0600

    Se trabaja en operaciones aritméticas básicas para trabajar en C#
    Tercer Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApp1
             Console.WriteLine("Valores:{0} -- {1}", añoActual, numeroPI);
             decimal montoFactura = 94593.34m + 0.07m;
             float impuestoVenta = 9239.04f;
+
+
             Console.WriteLine("Valores:{0} -- {1}", montoFactura, impuestoVenta);
             char letraInicial = 'P';
             string nombreCurso = letraInicial + "rogramación II";
@@ -19,6 +21,15 @@ namespace ConsoleApp1
             bool boolVerdadero = true;
             bool boolFalso = false;
             Console.WriteLine("Variables boleanas:{0} y {1}", boolVerdadero, boolFalso);
+
+            double resultadoSuma = 540 + -18.5;
+            float resultadoResta = 3458.35f - 334.34f;
+            decimal resultadoMultiplicacion = 23.423m * 3423m;
+            decimal resultadoDivision = 239.42m / 0.3m;
+            decimal resultadoResiduo = 2321.423m % 23m;
+            double resultadoPotencia = Math.Pow(78, 2);
+            double resultadoRaiz = Math.Sqrt(144);
+
 
         }
     }
